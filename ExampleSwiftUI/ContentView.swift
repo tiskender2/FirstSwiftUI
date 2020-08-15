cb58d72a6aa23ded6af57f1be137a7514ca0d4aa
Tolga İskender <tolga@basefy.com>
2020-08-15 18:58:21 +0300

    Initial Commit

---
+++ ExampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUI
+//
+//  Created by Tolga İskender on 15.08.2020.
+//  Copyright © 2020 Tolga İskender. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
