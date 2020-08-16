99152569d3c652a5a4f219b33e397e550da86ad7
Tolga İskender <tolga@basefy.com>
2020-08-16 11:51:37 +0300

    first commit

---
+++ ExampleSwiftUI/LoginUI/LoginUI.swift
@@ -0,0 +1,21 @@
+//
+//  LoginUI.swift
+//  ExampleSwiftUI
+//
+//  Created by Tolga İskender on 15.08.2020.
+//  Copyright © 2020 Tolga İskender. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginUI_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginUI()
+    }
+}
